cba60e09d2f23ea9f4989f4de8422f4cca3a464b
Nazarbek <safarovnazarbek7@gmail.com>
2025-10-24 22:00:35 +0500

    last

--- venv/gfg.py
+++ venv/gfg.py
@@ -1,4 +1,50 @@
-print('hello world')
-a = 15
-b = 15
-print(a+b)
+import psycopg2
+from psycopg2 import connect
+import psycopg2
+
+
+class Data_connect:
+    def __init__(self, dbname, user, password, host='localhost', port=5432):
+        self.connection = psycopg2.connect(
+            dbname=,
+            user=user,
+            password="Safarov2",
+            host=host,
+            port=port
+        )
+        self.cursor = self.connection.cursor()
+        self.cursor.close()
+
+    def add_car(self, Model, Title):
+        query = """
+        INSERT INTO cars (model, title)
+        VALUES (%s, %s)
+        """
+        self.cursor.execute(query, (Model, Title))
+        self.connection.commit()
+
+
+My_object = Data_connect('my_database', 'my_user', 'my_password')
+
+while True:
+    menu = input("menu: ")
+    if menu == "1":
+        model = input("model name: ")
+        title = input("title: ")
+        My_object.add_car(model, title)
+
+connecter = DB_connecter()
+connecter.Create_car("Model", "Title")
+
+def Menager_auto_salon():
+    while True:
+        code = input("code: ")
+
+        if code == "1":
+            Model = input("Madel: ")
+            Title = input("Title: ")
+            connecter.Create_car(Model, Title)
+
+
+
+# Menager_auto_salon()
